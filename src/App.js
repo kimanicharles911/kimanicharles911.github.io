8c1b19e16eb780dd131cd5e0f1fe5c34ec52076d
kimanicharles911 <kimanicharles911@gmail.com>
2021-09-01 17:40:44 +0300

    changed json file path

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   const [resumeData, setResumeData] = useState({});
 
   useEffect(() => {
-    fetch("/resumeData.json")
+    fetch("./resumeData.json")
       .then((res) => res.json())
       .then((data) => {
         setResumeData(data);
